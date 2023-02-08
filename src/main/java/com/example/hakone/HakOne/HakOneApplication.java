430179f2636992dc2d6b84a5c9f9c2cd6a7bf785
kryptonite43 <sonyj0430@gmail.com>
2023-02-08 18:28:35 +0900

    initial commit

---
+++ src/main/java/com/example/hakone/HakOne/HakOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.hakone.HakOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HakOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hakone/HakOne/HakOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hakone.HakOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
