ee0d30d4cfd5064c9f0ac54ff1e08443286788b3
kryptonite43 <sonyj0430@gmail.com>
2023-03-23 02:40:08 +0900

    fix:> spring batch jobexecution 해결

--- src/main/java/com/example/hakone/HakOne/dbset/FileItemReaderJobConfig.java
+++ src/main/java/com/example/hakone/HakOne/dbset/FileItemReaderJobConfig.java
@@ -5,17 +5,34 @@ import com.example.hakone.HakOne.domain.classroom.Classroom;
 import com.example.hakone.HakOne.dto.CsvDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class FileItemReaderJobConfig {
+    @Autowired
+    private JobExplorer jobExplorer;
+    @Autowired
+    private JobOperator jobOperator;
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final CsvReader csvReader;
@@ -29,7 +46,8 @@ public class FileItemReaderJobConfig {
 
 
     @Bean
-    public Job csvFileItemReaderJob(){
+    public Job csvFileItemReaderJob() throws Exception {
+        stopJobExecution(1);
         return jobBuilderFactory.get("csvFileItemReaderJob")
                 .start(csvFileItemReaderStep1())
                 .next(csvFileItemReaderStep2())
@@ -62,5 +80,37 @@ public class FileItemReaderJobConfig {
                 .processor(academyClassProcessor)
                 .writer(academyClassWriter)
                 .build();
+    }
+
+
+
+    // 중지할 JobExecution의 ID를 파라미터로 받는다.
+    public void stopJobExecution(long jobExecutionId) throws Exception {
+        // 현재 실행중인 JobExecution 객체를 조회한다.
+        JobExecution jobExecution = jobExplorer.getJobExecution(jobExecutionId);
+        if (jobExecution != null) {
+            // 해당 JobExecution의 JobInstance 객체를 조회한다.
+            JobInstance jobInstance = jobExecution.getJobInstance();
+            if (jobInstance != null) {
+                // 트랜잭션 정의를 생성한다.
+                DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+                def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+                TransactionStatus status = transactionManager.getTransaction(def);
+                try {
+                    // 현재 실행중인 JobExecution을 중지시킨다.
+                    jobOperator.stop(jobExecutionId);
+                    // 해당 JobExecution의 상태를 STOPPING으로 변경한다.
+                    jobExecution.setStatus(BatchStatus.STOPPING);
+                    // JobExecution을 업데이트한다.
+                    jobRepository.update(jobExecution);
+                    // 트랜잭션을 커밋한다.
+                    transactionManager.commit(status);
+                } catch (Exception e) {
+                    // 트랜잭션을 롤백한다.
+                    transactionManager.rollback(status);
+                    throw e;
+                }
+            }
+        }
     }
 }
